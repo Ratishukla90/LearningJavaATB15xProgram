782f331ec71f9bad1702ae82208da2a4f9520620
ratishukla90 <ratishukla90@gmail.com>
2025-12-24 08:21:05 +0530

    task 17th dec 2025

---
+++ src/Dec2025/ex_24122025/Lab001.java
@@ -0,0 +1,15 @@
+package Dec2025.ex_24122025;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        int n1 = 2;
+
+        int n2 = 9;
+
+        int n3 = -11;
+
+        int result=(n1>n2? n1:((n2>n3)?n2:n3));
+        System.out.println(result);
+    }
+}
