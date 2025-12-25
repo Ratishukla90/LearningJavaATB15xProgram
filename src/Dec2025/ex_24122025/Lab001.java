98b72eb41ea1eb89b15a08a841d9cc84a94007a5
Rati Shukla <ratishukla90@gmail.com>
2025-12-25 22:49:53 +0530

    Update Lab001.java

--- src/Dec2025/ex_24122025/Lab001.java
+++ src/Dec2025/ex_24122025/Lab001.java
@@ -9,7 +9,7 @@ public class Lab001 {
 
         int n3 = -11;
 
-        int result=(n1>n2? n1:((n2>n3)?n2:n3));
+        int result=((n1>n2 && n1>n3)? n1:((n2>n3)?n2:n3));
         System.out.println(result);
     }
 }
